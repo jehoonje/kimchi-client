605f9fc07f6c60b32bf09fa18c8645faf67cce95
Jehoon <limjhoon8@gmail.com>
2025-02-01 21:53:26 +0900

    💄 Style : 메인타이틀 gsap 적용

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,34 @@
 // src/pages/Home.jsx
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import gsap, { Expo } from "gsap";
 import styles from "../styles/ConicDemo.module.scss";
 import "../styles/SearchBar.scss";
-// SCSS 모듈 import (전역 x, 로컬 스코프)
 
 function Home() {
+  const titleRef = useRef(null);
+
+  useEffect(() => {
+    // 페이지 진입 시 h1 텍스트가 50px 아래에서 시작하여 원래 위치로 이동하며 페이드인됨.
+    gsap.fromTo(
+      titleRef.current,
+      { opacity: 0, y: 80 },
+      { opacity: 1, y: 0, duration: 3, ease: Expo.easeOut }
+    );
+  }, []);
+
   return (
     <div className={styles["conic-container"]}>
-      {/* 원본 Jade: header → 여기서는 div.conic-header 로 */}
       <div className={styles["conic-header"]}>
-        <h1 className={styles["conic-title"]}>
+        <h1 className={styles["conic-title"]} ref={titleRef}>
           Explore and enjoy the taste of Korean food.
         </h1>
         <em className={styles["conic-subtitle"]}>What’s on Your Plate?</em>
         {/* 검색 UI */}
         <div className="search-wrapper">
-          {/* 검색 버튼 (토글) */}
           <input id="search-btn" type="checkbox" />
-
-          {/* 돋보기 아이콘 (라벨) */}
           <label htmlFor="search-btn">
             <span className="visually-hidden">Toggle Search</span>
           </label>
-
-          {/* 검색 입력창 */}
           <input id="search-bar" type="text" placeholder="Search..." />
         </div>
       </div>
